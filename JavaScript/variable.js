7367ab45d8213ef49cfdf832bcfa456621894f23
agrimaagrawal2 <agrima.agrawal_cs23@gla.ac.in>
2025-11-16 22:29:38 +0530

    Add files via upload

---
+++ JavaScript/variable.js
@@ -0,0 +1,27 @@
+console.log("Welcome to javascript programming");
+let a=3.14
+console.log(a)
+
+let c=Symbol('id')
+console.log(c)
+
+let d
+console.log(d)
+
+let b=null
+console.log(b)
+
+console.log("hello world")
+
+let arr1=[1,2,3,4,5]
+console.log(arr1[2])
+
+let arr={"name":'mahi',"id":'10'}
+console.log(arr)
+
+// object
+let arr2={name:'mahi',id:10,age:25}
+console.log(arr2.age)
+
+console.log(bal)
+let bal=10;

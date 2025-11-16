f120493a4d03fd61915e37bfece80a276d3f773a
agrimaagrawal2 <agrima.agrawal_cs23@gla.ac.in>
2025-11-16 22:07:09 +0530

    Add files via upload

---
+++ Intro to JavaScript/login greetingg.js
@@ -0,0 +1,15 @@
+let userName = "Agrima";  
+let currentHour = new Date().getHours();
+
+// Check time and display greeting
+let greetingMessage;
+
+if (currentHour < 12) {
+    greetingMessage = `Good Morning ${userName}!`;
+} else if (currentHour >= 12 && currentHour < 17) {
+    greetingMessage = `Good Afternoon ${userName}!`;
+} else {
+    greetingMessage = `Good Evening ${userName}!`;
+}
+
+console.log(greetingMessage);
